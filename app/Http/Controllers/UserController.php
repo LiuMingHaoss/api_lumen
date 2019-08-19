d7a67d0e800d370d44a7d5b6f7df65bfb53bf656
liu <461043365@qq.com>
2019-08-19 02:36:09 +0000

    token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,17 +9,21 @@ use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Str;
 class UserController extends BaseController
 {
+    /**
+     * @param Request $request
+     * @return false|string
+     * 用户登录
+     */
     public function login(Request $request)
     {
         //接收登录数据
-        $username=$request->input('username');      //用户名
-        $pwd=$request->input('pwd');                //用户密码
-        $email=$request->input('email');            //用户邮箱
+        $data=file_get_contents('php://input');;
+        $data=json_decode($data,JSON_UNESCAPED_UNICODE);
 
-        $userInfo=DB::table('api_user')->where('email',$email)->first();    //根据邮箱查询用户数据
+        $userInfo=DB::table('api_user')->where('email',$data['email'])->first();    //根据邮箱查询用户数据
 
         if($userInfo){      //用户存在
-            if($pwd==$userInfo->pwd){   //密码正确
+            if($data['pwd']==$userInfo->pwd){   //密码正确
                 $key="token:uid:".$userInfo->id;
                 $token=Redis::get($key);
                 if(!$token){                //token过期
@@ -46,5 +50,15 @@ class UserController extends BaseController
         }
 
         return json_encode($response,JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * 个人中心
+     */
+    public function my(Request $request)
+    {
+        $uid=$request->input('uid');
+        $userInfo=DB::table('api_user')->where('id',$uid)->first();
+        echo "<pre>";print_r($userInfo);echo"</pre>";
     }
 }
